14716fb93a8cd7b927eb942c4b6dbdc4be0854ff
ckm960411 <a01098956117@gmail.com>
2023-09-03 01:06:51 +0900

    NavbarCategories 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import NavbarCategories from './NavbarCategories';
 
 export default function Header() {
   return (
     <nav>
       <Navbar />
+      <NavbarCategories />
     </nav>
   );
 }
