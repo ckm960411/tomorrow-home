031fdb3f78e0af38ad8ed0e843f47512f99423ad
ckm960411 <a01098956117@gmail.com>
2023-09-20 01:02:05 +0900

    slug 로 상품 데이터 받아오기

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,17 @@
+import { getProductById } from '@/service/get-product';
+import { Product } from '@/service/get-today-deals';
 import React from 'react';
 
-export default function ProductPage({
+export default async function ProductPage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
-  return <div className="max-w-[1256px] mx-auto px-[60px]">ProductPage</div>;
+  const product: Product = await getProductById(slug);
+
+  if (!product) {
+    return <div>404 NOT FOUND</div>;
+  }
+
+  return <div className="max-w-[1256px] mx-auto px-[60px]">{product.name}</div>;
 }
