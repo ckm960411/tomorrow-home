78b519ade6ba59a3837f967470d39e18443c364f
ckm960411 <a01098956117@gmail.com>
2023-09-20 00:24:20 +0900

    ProductOrder 변경 구현

--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -10,6 +10,7 @@ export default function ProductsContainer() {
   const [productOrder, setProductOrder] = useState<ProductOrder>(
     ProductOrder.popular
   );
+  const [showPopup, setShowPopup] = useState(false);
 
   return (
     <div className="max-w-[1256px] mx-auto px-[60px] mt-[40px] flex flex-col gap-[16px]">
@@ -19,11 +20,34 @@ export default function ProductsContainer() {
           <span className="text-gray-600 text-[14px]">배송</span>
           <FiChevronDown className="text-gray-600" />
         </div>
-        <div className="flex items-center gap-[1px]">
+        <div
+          onMouseEnter={() => setShowPopup(true)}
+          className="flex items-center gap-[1px] cursor-pointer relative"
+        >
           <span className="text-gray-500 text-[12px]">
             {getProductOrderKorean(productOrder)}
           </span>
           <GoTriangleDown className="text-gray-500" />
+          {showPopup && (
+            <ul
+              onMouseLeave={() => setShowPopup(false)}
+              className="absolute top-full right-0 w-[200px] bg-white rounded-xl shadow-2xl"
+            >
+              {Object.keys(ProductOrder).map((po, i) => (
+                <li
+                  key={i}
+                  onClick={() => setProductOrder(po as ProductOrder)}
+                  className={`px-[16px] py-[12px] hover:bg-main hover:bg-opacity-10 text-[14px] ${
+                    po === productOrder
+                      ? 'text-main font-bold'
+                      : 'text-gray-600 font-medium'
+                  }`}
+                >
+                  {getProductOrderKorean(po as ProductOrder)}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
       <ProductList productOrder={productOrder} />
